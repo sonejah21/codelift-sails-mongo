dff4de756fff34fe2c7ce798d676f3cf589999d0
voxlol <voxlol@outlook.com>
2016-03-08 16:11:09 -0600

    Use DATABASE_URL env variable for mongodb config

--- config/connections.js
+++ config/connections.js
@@ -58,8 +58,9 @@ module.exports.connections = {
   ***************************************************************************/
   someMongodbServer: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
+    url: process.env.DATABASE_URL
+    // host: 'localhost',
+    // port: 27017,
     // user: 'username',
     // password: 'password',
     // database: 'your_mongo_db_name_here'
